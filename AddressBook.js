cd8446bf17b5316c1be10d5b65a32af0ca532da8
Your Name <you@example.com>
2023-02-19 11:24:19 +0530

    Added: create addressbook with firstname,lastname,address,city,zip,state,zip,phone no.,Email

--- AddressBook.js
+++ AddressBook.js
@@ -1,4 +1,33 @@
-
 //Printing a welcome note to console
 console.log("Welcome to address book system!");
+
+class AddressBookContact {
+
+    constructor (firstName, lastName, address, city, state, zipCode, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    toString() {
+        return "firstName="+this.firstName+" ,lastName="+this.lastName+
+        " ,address="+this.address+" ,city="+this.city+" ,state="+this.state+
+        " ,zip="+this.zipCode+" ,phoneNumber="+this.phoneNumber+" ,email="+this.email;
+    }
+}
+
+/**
+ * Created a new contact
+ */
+let addressBook = new AddressBookContact("Poonam", "Lohar", "Sambhaji Chowk", 
+    "kolhapur", "Maharashtra", 416103, "9089786787", "poonam@gmail.com");
+
+//Printing the added contact to console
+console.log(addressBook.toString());
+
+
 
